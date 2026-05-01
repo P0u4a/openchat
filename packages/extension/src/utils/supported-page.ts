e3bc983037650a18ecee571f4a577e8de174c1b9
p0u4a <pouyamiria@gmail.com>
2026-05-01 14:22:29 +0930

    remove dead code

--- packages/extension/src/utils/supported-page.ts
+++
@@ -1,7 +0,0 @@
-import { CLAUDE_ORIGIN, CHATGPT_ORIGIN } from "../lib/constants.js";
-
-const SUPPORTED_ORIGINS = [CLAUDE_ORIGIN, CHATGPT_ORIGIN];
-
-export function isSupportedPage(url: string): boolean {
-  return SUPPORTED_ORIGINS.some((origin) => url.startsWith(origin));
-}
